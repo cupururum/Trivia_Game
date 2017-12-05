bcc14f0cbbbe0fff87109fb3b125474ec466be70
cupururum <cupururum@Vogon-2.local>
2017-12-05 10:58:08 -0800

    playing with JSON

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,10 @@ var url = "https://cupururum.github.io/Trivia_Game/assets/javascript/questions.j
 $.getJSON(url, function(data){
   console.log(data);
   var $question = $("#question")
-  var questionHTML = $question.text(data.question);
+  var questionHTML = $question.text(data[0].question);
   var answersHTML = "<ul>";
-  console.log(data.answers)
-  $.each(data.answers, function(i, answer) {
+  console.log(data[0].answers)
+  $.each(data[0].answers, function(i, answer) {
     answersHTML += '<li class="answer">';
     answersHTML += answer + '</li>';
   });
