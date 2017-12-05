c09d6f444e4d49cb3e474260bfc231e1de445f43
cupururum <cupururum@Vogon-2.local>
2017-12-04 23:30:50 -0800

    playing with JSON

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var url = "../data/questions.json";
+$.getJSON(url, function(data){
+  
+  var $question = $("#question")
+  var questionHTML = $question.text(data.question);
+  var answersHTML = "<ul>";
+  $.each(data.answers, function(i, answer) {
+    answersHTML += '<li class="answer">'
+    answersHTML += answer + '</li>'
+  });
+  $('#variants').html(answersHTML);
+}); //end JSON
