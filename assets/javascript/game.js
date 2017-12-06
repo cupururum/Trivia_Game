7282e9583ece15c560dd727691ea7ce03911da6d
cupururum <cupururum@Vogon-2.local>
2017-12-05 16:16:53 -0800

    playing with JSON

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,7 @@
+var index = 0;
+var wrong = 0;
+var correct = 0;
+var unanswered = 0;
 //when user presses the button game starts
 $('button').on("click", function(){
   //the question and answers appear
@@ -6,30 +10,37 @@ $('button').on("click", function(){
 
 //when game starts:
 
-var index = 0;
+
 function chooseQuestion() {
 
-  var url = "https://cupururum.github.io/Trivia_Game/assets/javascript/questions.json";
+  var timer = 10;
+  $("#timer").text(timer);
+  var intervals = setInterval(myTimer, 1000);
+  function myTimer() {
+    timer--;
+    $("#timer").text(timer);
+    console.log(timer);
+    if (timer === 0) {
+      clearInterval(intervals);
+      index++
+      unanswered++
+      chooseQuestion();
+     }
+    }; //end of myTimer
 
+  var url = "https://cupururum.github.io/Trivia_Game/assets/javascript/questions.json";
   $.getJSON(url, function(data){
     //the countdoun timer appears
-    var timer = 5;
-    $("#timer").text(timer);
-    var intervals = setInterval(myTimer, 1000);
-    function myTimer() {
-      timer--;
-      $("#timer").text(timer);
-      console.log(timer);
-      if (timer === 0) {
-        $("#timer").text(timer);
-        clearInterval(intervals);
 
-        chooseQuestion();
-          if (index === data.length) {
-            clearInterval(intervals);
-          };
-      }
-    }; //end of myTimer
+
+
+    if (index === data.length) {
+        clearInterval(intervals);
+        $("#question").empty();
+        $("#variants").append("<div>").html('<p> Correct: ' + correct + '<br> Wrong: ' + wrong + '<br> Unanswered: ' + unanswered + '</p>')
+        index = 0;
+        setTimeout(chooseQuestion, 1000*5);
+      };
 
       //the question and answers appear
     console.log(data);
@@ -45,7 +56,27 @@ function chooseQuestion() {
     answersHTML += "</ul>";
     $('#variants').html(answersHTML);
 
+    $(".answer").on("click", function(){
+      clearInterval(intervals);
+      var userAnswer = $(this).text()
+      console.log(userAnswer);
+      console.log(data[index].correctAnswer);
+      if (userAnswer === data[index].correctAnswer) {
+        index++
+        correct++
+        $("#variants").append('<div>').html("<h2> You chose the correct answer </h2>");
+        setTimeout(chooseQuestion, 1000*3)
+      } else if (userAnswer !== data[index].correctAnswer) {
+        index++
+        wrong++
+        $("#variants").append('<div>').html("<h2> You chose the wrong answer </h2>");
+        setTimeout(chooseQuestion, 1000*3)
+      }
+     });
+
+
   }); //end JSON
+
 };// end of chooseQuestion
 
 
