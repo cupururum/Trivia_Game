71403fcdb491381714de656238a41b4f9fd11f0f
cupururum <cupururum@Vogon-2.local>
2017-12-08 11:41:13 -0800

    gyphs added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,7 @@ $('button').on("click", function(){
 
 
 function chooseQuestion() {
-
+//the countdoun timer appears
   var timer = 10;
   $("#timer").text(timer);
   var intervals = setInterval(myTimer, 1000);
@@ -30,14 +30,10 @@ function chooseQuestion() {
 
   var url = "https://cupururum.github.io/Trivia_Game/assets/javascript/questions.json";
   $.getJSON(url, function(data){
-    //the countdoun timer appears
-
-
-
+    //check here if the json array is over, then show the result and continue game
     if (index === data.length) {
         clearInterval(intervals);
-        $("#question").empty();
-        $("#variants").append("<div>").html('<p> Correct: ' + correct + '<br> Wrong: ' + wrong + '<br> Unanswered: ' + unanswered + '</p>')
+        $(".gameDiv").append('<div>')html('<p> Correct: ' + correct + '<br> Wrong: ' + wrong + '<br> Unanswered: ' + unanswered + '</p>')
         index = 0;
         setTimeout(chooseQuestion, 1000*5);
       };
@@ -45,38 +41,35 @@ function chooseQuestion() {
       //the question and answers appear
     console.log(data);
     var $question = $("#question")
-    var questionHTML = $question.text(data[index].question);
+    var questionHTML = $question.text(data[index].question); //display the question
     console.log(data[index].question);
-    var answersHTML = "<ul>";
+    var answersHTML = "<ul class='answer-list'>"; //start building the list with answers
     console.log(data[index].answers)
     $.each(data[index].answers, function(i, answer) {
-      answersHTML += '<li class="answer">';
-      answersHTML += answer + '</li>';
+      answersHTML += '<li class="answer">'; //creating list of items
+      answersHTML += answer + '</li>'; //close the item
     });
-    answersHTML += "</ul>";
-    $('#variants').html(answersHTML);
+    answersHTML += "</ul>"; //close the list
+    $('#variants').html(answersHTML);//display list in html
 
-    $(".answer").on("click", function(){
-      clearInterval(intervals);
+    $(".answer-list").on("click", '.answer', function(){ //assign the click function to the list items
+      clearInterval(intervals); //when I choose the answer I stop timer
       var userAnswer = $(this).text()
       console.log(userAnswer);
       console.log(data[index].correctAnswer);
-      if (userAnswer === data[index].correctAnswer) {
+      if (userAnswer === data[index].correctAnswer) { //check if answer is correct
         index++
         correct++
         $("#variants").append('<div>').html("<h2> You chose the correct answer </h2>");
         setTimeout(chooseQuestion, 1000*3)
-      } else if (userAnswer !== data[index].correctAnswer) {
+      } else if (userAnswer !== data[index].correctAnswer) { //if the answer is not correct
         index++
         wrong++
         $("#variants").append('<div>').html("<h2> You chose the wrong answer </h2>");
         setTimeout(chooseQuestion, 1000*3)
       }
-     });
-
-
+    }); //end of onclick function
   }); //end JSON
-
 };// end of chooseQuestion
 
 
